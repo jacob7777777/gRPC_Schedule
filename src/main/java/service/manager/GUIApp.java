6336a63d4be5263686c29bd48473a0687017b438
Jacob <x18157807@student.ncirl.ie>
2022-04-25 11:57:40 +0100

    GUI starting to have shape

--- src/main/java/service/manager/GUIApp.java
+++ src/main/java/service/manager/GUIApp.java
@@ -22,35 +22,148 @@ import io.grpc.ManagedChannelBuilder;
 public class GUIApp implements ActionListener{
 
 
-	private JTextField entry1, reply1;
+	//private JTextField entry1, reply1;
+	private JTextField entry1;
+	private JTextField entry12;
+	private JTextField entry13;
+	private JTextField entry14;
+	private JTextField entry15;
+	private JTextField entry16;
+	private JTextField entry17;
+	
 	private JTextField entry2, reply2;
 	private JTextField entry3, reply3;
 	
+	private JTextArea reply1;
 	
+	//https://www.youtube.com/watch?v=ohNqQagkDDY
+	//https://stackoverflow.com/questions/7858752/in-java-how-do-you-add-a-jpanel-to-another-jpanel
+	//https://www.youtube.com/watch?v=Kl3klve_rmQ
+	//https://www.youtube.com/watch?v=dvzAuq-YDpM
 	private JPanel getService1JPanel() {
 
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.gray);
+		//JPanel innerPanel = new JPanel();
+		//JPanel innerPanel2 = new JPanel();
+		//JPanel innerPanel3 = new JPanel();
 		
 		
-		BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.X_AXIS);
+		//BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints appGrid = new GridBagConstraints();	
+		appGrid.fill = GridBagConstraints.HORIZONTAL;
+		appGrid.insets = new Insets(5, 5, 5, 5);
+		//BoxLayout boxlayout = new BoxLayout(innerPanel, BoxLayout.LINE_AXIS);
+		//BoxLayout boxlayout2 = new BoxLayout(innerPanel2, BoxLayout.Y_AXIS);
+		//BoxLayout boxlayout3 = new BoxLayout(innerPanel3, BoxLayout.Y_AXIS);
 		
-		JLabel label = new JLabel("Enter value")	;
-		panel.add(label);
-		panel.add(Box.createRigidArea(new Dimension(10, 0)));
-		entry1 = new JTextField("",10);
-		panel.add(entry1);
-		panel.add(Box.createRigidArea(new Dimension(10, 0)));
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		JLabel label = new JLabel("Card Number: ")	;
+		appGrid.gridx = 0;
+		appGrid.gridy = 0;
+		panel.add(label, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry1 = new JTextField("",5);
+		appGrid.gridx = 0;
+		appGrid.gridy = 1;
+		panel.add(entry1, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+		JLabel label1 = new JLabel("Task: ")	;
+		appGrid.gridx = 0;
+		appGrid.gridy = 2;
+		panel.add(label1, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry12 = new JTextField("",10);
+		appGrid.gridx = 0;
+		appGrid.gridy = 3;
+		panel.add(entry12, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
 
-		JButton button = new JButton("Invoke Service 1");
+		JLabel CapProf = new JLabel("Capacity profile")	;
+		appGrid.gridx = 1;
+		appGrid.gridy = 0;
+		panel.add(CapProf, appGrid);
+		
+		JLabel CapProfMon = new JLabel("Monday")	;
+		appGrid.gridx = 1;
+		appGrid.gridy = 1;
+		panel.add(CapProfMon, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry13 = new JTextField("",10);
+		appGrid.gridx = 2;
+		appGrid.gridy = 1;
+		panel.add(entry13, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+		JLabel CapProfTue = new JLabel("Tuesday")	;
+		appGrid.gridx = 1;
+		appGrid.gridy = 2;
+		panel.add(CapProfTue, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry14 = new JTextField("",10);
+		appGrid.gridx = 2;
+		appGrid.gridy = 2;
+		panel.add(entry14, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+		JLabel CapProfWed = new JLabel("Wednesday")	;
+		appGrid.gridx = 1;
+		appGrid.gridy = 3;
+		panel.add(CapProfWed, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry15 = new JTextField("",10);
+		appGrid.gridx = 2;
+		appGrid.gridy = 3;
+		panel.add(entry15, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+		JLabel CapProfThu = new JLabel("Thursday")	;
+		appGrid.gridx = 1;
+		appGrid.gridy = 4;
+		panel.add(CapProfThu, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry16 = new JTextField("",10);
+		appGrid.gridx = 2;
+		appGrid.gridy = 4;
+		panel.add(entry16, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+		JLabel CapProfFri = new JLabel("Friday")	;
+		appGrid.gridx = 1;
+		appGrid.gridy = 5;
+		panel.add(CapProfFri, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		entry17 = new JTextField("",10);
+		appGrid.gridx = 2;
+		appGrid.gridy = 5;
+		panel.add(entry17, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
+		
+
+		JButton button = new JButton("Introduce profile");
 		button.addActionListener(this);
-		panel.add(button);
-		panel.add(Box.createRigidArea(new Dimension(10, 0)));
+		appGrid.gridx = 1;
+		appGrid.gridy = 7;
+		panel.add(button, appGrid);
+		//panel.add(Box.createRigidArea(new Dimension(10, 10)));
 
-		reply1 = new JTextField("", 10);
+		//reply1 = new JTextField("", 10);
+		reply1 = new JTextArea(3, 20);
 		reply1 .setEditable(false);
-		panel.add(reply1 );
-
-		panel.setLayout(boxlayout);
+		appGrid.gridx = 2;
+		appGrid.gridy = 7;
+		panel.add(reply1, appGrid);
+		
+		//panel.setLayout(boxlayout);
+		//innerPanel.setLayout(boxlayout);	
+		//innerPanel2.setLayout(boxlayout2);	
+		//innerPanel3.setLayout(boxlayout3);
+		
+		//panel.add(innerPanel);
+		//panel.add(innerPanel2);
+		//panel.add(innerPanel3);
 
 		return panel;
 
@@ -122,8 +235,9 @@ public class GUIApp implements ActionListener{
 
 	private void build() { 
 
-		JFrame frame = new JFrame("Service Controller Sample");
+		JFrame frame = new JFrame("GUI Application with the 3 services");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 
 		// Set the panel to add buttons
 		JPanel panel = new JPanel();
